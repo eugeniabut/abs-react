1ca5fd4023bf0b3be427fa59e425175e928212a7
ebutkev <eugeniabutkevich2022@gmail.com>
2024-06-15 20:31:20 +0200

    add, delete, edit job

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+
 import { 
 createBrowserRouter, 
   RouterProvider, 
@@ -12,23 +14,50 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
+
+
+
+const App = () => {
+
+ 
+const addJob = async(newJob)=>{
+const res = await fetch("/api/jobs", {
+ method: "POST",
+ headers: {
+  "Content-Type": "application/json"
+ },
+ body: JSON.stringify(newJob)
+})
+return
+}
+
+const deleteJob = async(id)=>{
+  const res = await fetch( `/api/jobs/${id}` ,{
+    method: "DELETE"
+  })
+  return
+}
+
+const editJob =()=>{}
 
 const router = createBrowserRouter( 
+
+
   createRoutesFromElements (
     <Route path='/' element = {<MainLayout />}>
        <Route index element = {<HomePage/>}/>
        <Route path='/jobs' element = {<JobsPage/>}/>
-       <Route path='/jobs/:id' element = {<JobPage/> } loader = {jobLoader}/>
-       <Route path='/add-job' element = {<AddJobPage/>}/>
+       <Route path='/jobs/:id' element = {<JobPage  deleteJob ={deleteJob} /> } loader = {jobLoader}/>
+       <Route path='/add-job' element = {<AddJobPage  addJobSubmit = {addJob}/>}/>
+       <Route path='/edit-job/:id' element = {<EditJobPage editJobSubmit={editJob} />}/>
+
        <Route path='*' element = {<NotFoundPage/>}/>
 </Route>
   )
 
 )
-
-const App = () => {
-
   return ( 
   <RouterProvider router ={router}/>
   )
--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const AddJobPage = () => {
+const AddJobPage = ({addJobSubmit}) => {
     const [title, setTitle] = useState("")
     const [type,setType] = useState("Full-Time")
     const [location,setLocaion] = useState("") 
@@ -12,6 +13,8 @@ const AddJobPage = () => {
    
     const [contactEmail,setContactEmail] = useState("") 
     const [contactPhone,setContactPhone] = useState("")
+
+    const navigate = useNavigate()
 
     const submitForm =(e)=>{
          e.preventDefault()
@@ -26,10 +29,11 @@ const AddJobPage = () => {
                 name:companyName,
                 description:companyDescription,
                 contactEmail,
-                contactPhone
+                contactPhone,
             }
          } 
-         console.log(newJob); 
+         addJobSubmit(newJob); 
+         return navigate("/jobs")
     }
 
        
